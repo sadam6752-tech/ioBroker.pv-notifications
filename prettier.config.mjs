cf0ed9b01fdfc3e7a50c2483e53a5102378d34a8
Alex1808 <sadam6752@gmail.com>
2026-06-20 12:20:25 +0200

    v1.2.25: fix W5604 weather i18n keys, engines.node >=22, workflow node 24, devDeps update, tsconfig.json, prettier.config.mjs

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,8 +1,3 @@
-// iobroker prettier configuration file
-import prettierConfig from '@iobroker/eslint-config/prettier.config.mjs';
-
-export default {
-    ...prettierConfig,
-    // uncomment next line if you prefer double quotes
-    // singleQuote: false,
-}
+// prettier.config.mjs
+// This file is required by @iobroker/eslint-config
+export { default } from '@iobroker/eslint-config/prettier.config.mjs';
